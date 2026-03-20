b8fff854c51c14df7e37ec9201d0dea014512665
user.email <minxie.dallas@gmail.com>
2026-03-20 09:35:39 -0500

    Add rocket emoji favicon to generated SEO pages

--- build.js
+++ build.js
@@ -95,6 +95,7 @@ function generatePostPage(post, renderedHtml) {
 + '    <title>' + escapedTitle + ' \u2014 ' + SITE_NAME + '</title>\n'
 + '    <meta name="description" content="' + escapedExcerpt + '">\n'
 + '    <link rel="canonical" href="' + postUrl + '">\n'
++ '    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>\uD83D\uDE80</text></svg>">\n'
 + '\n'
 + '    <!-- Open Graph -->\n'
 + '    <meta property="og:title" content="' + escapedTitle + '">\n'
